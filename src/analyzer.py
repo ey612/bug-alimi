7b3ca45a30ef64104680c3cbff345c2a72f02619
chaessi <chaessi514@gmail.com>
2026-03-15 23:58:37 +0900

    init: 프로젝트 초기 구조 설정

---
+++ src/analyzer.py
@@ -0,0 +1,2 @@
+def analyze_bug_with_ai(bug_title, bug_description):
+    pass
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from analyzer import analyze_bug_with_ai
+from notifier import send_webhook_message
+
+
+if __name__ == "__main__":
+    print("버그알리미 실행!")
